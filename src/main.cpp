b8702f4c831647a2e5c1eb2d0d21528a7c19ea2e
Jay <jayay98@gmail.com>
2024-11-20 22:22:04 +0800

    Functional Hello World importing all submodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <Eigen/Dense>
+#include <sndfile.hh>
+#include <matplotlibcpp.h>
+namespace plt = matplotlibcpp;
 
 int main(void)
 {
+    Eigen::Matrix3d m;
+    m << 1,2,3,4,5,6,7,8,9;
     std::cout << "Hello World!" << std::endl;
+    std::cout << m << std::endl;
+    std::cout << sf_version_string() << std::endl;
+    
+    plt::plot({1,2,3,4});
+    plt::show();
+
     return 0;
 }
